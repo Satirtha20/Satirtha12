c0d36d0de0565da73b308ae7f692e59f01dcd279
Satirtha Chowdhury <89466949+Satirtha20@users.noreply.github.com>
2021-11-03 13:15:42 +0600

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,11 @@ import yagmail
 import re
 from re import search
 import smtplib
- 
+
 import streamlit as st
 import streamlit.components.v1 as components
 from streamlit import caching
- 
+
 import plotly.express as px
 import plotly.figure_factory as ff
 import plotly.graph_objects as go
@@ -40,47 +40,48 @@ st.set_page_config(
 #                       auth_plugin='mysql_native_password')
 #     cursor = db.cursor()
 #     return cursor, db
- 
+
 cursor, db = get_database_connection()
- 
+
+
 # cursor.execute("SHOW DATABASES")
- 
+
 # databases = cursor.fetchall() ## it returns a list of all databases present
- 
+
 # st.write(databases)
- 
+
 # cursor.execute('''CREATE TABLE information (id varchar(255),
 #                                               studentname varchar(255),
-#                                               re_date date,
+#                                               reg_date date,
 #												status varchar(255))''')
 # cursor.execute("Select * from information")
 # tables = cursor.fetchall()
 # st.write(tables)
 
 def admin():
-    username=st.sidebar.text_input('Username',key='user')
-    password=st.sidebar.text_input('Password',type='password',key='pass')
-    st.session_state.login=st.sidebar.checkbox('Login')
- 
-    if st.session_state.login==True:
-        if username=="name" and password=='password':
+    username = st.sidebar.text_input('Username', key='user')
+    password = st.sidebar.text_input('Password', type='password', key='pass')
+    st.session_state.login = st.sidebar.checkbox('Login')
+
+    if st.session_state.login == True:
+        if username == "name" and password == 'password':
             st.sidebar.success('Login Success')
 
-            date1=st.date_input('Date1')
-            date2=st.date_input('Date2')
-            cursor.execute(f"select * from information where re_date between '{date1}' and '{date2}'")
+            date1 = st.date_input('Date1')
+            date2 = st.date_input('Date2')
+            cursor.execute(f"select * from information where reg_date between '{date1}' and '{date2}'")
             # db.commit()
-            tables =cursor.fetchall()
+            tables = cursor.fetchall()
             # st.write(tables)
             for i in tables:
+                st.write(i[0])
                 st.write(i[1])
-                st.write(i[2])
-                Accept=st.button('Accept',key=i[0])
+                Accept = st.button('Accept', key=i[0])
                 if Accept:
                     st.write('Accepted')
                     cursor.execute(f"Update information set status='Accepted' where id='{i[0]}'")
                     db.commit()
-                Reject=st.button('Reject',key=i[0])
+                Reject = st.button('Reject', key=i[0])
                 if Reject:
                     st.write('Rejected')
                     cursor.execute(f"Update information set status='Rejected' where id='{i[0]}'")
@@ -91,54 +92,64 @@ def admin():
 
 
 def form():
-    id=uuid4()
-    id=str(id)[:10]
+    id = uuid4()
+    id = str(id)[:10]
     with st.form(key='member form'):
-        sname=st.text_input('Student Name')
-        re_date=st.date_input('Registration Date')
-        status='In Progress'
+        studentname = st.text_input('Student Name')
+        email = st.text_input('E-mail')
+        contact_no = st.text_input('Contact')
+        institution = st.text_input('College/University')
+        semester_year = st.text_input('Semester/Year')
+        grade = st.text_input('GPA/CGPA')
+        reg_date = st.date_input('Registration Date')
+        status = 'In Progress'
         if st.form_submit_button('Submit'):
-            query = f'''INSERT INTO information (id,studentname,
-                                                re_date,status) VALUES ('{id}','{sname}',
-                                                '{re_date}','{status}')'''
+            query = f'''INSERT INTO information (id,studentname,email,contact_no,institution,semester_year,grade,
+                                                reg_date,status) VALUES ('{id}','{studentname}','{email}','{contact_no}','{institution}',
+                                                '{semester_year}','{grade}','{reg_date}','{status}')'''
             cursor.execute(query)
             db.commit()
-            st.success(f'Congratulation *{sname}*! You have successfully Registered')
+            st.success(f'Congratulation *{studentname}*! You have successfully Registered')
             st.code(id)
             st.warning("Please Store this code!!!")
-        
+
+
 def info():
-    id=st.text_input('Your Code')
-    Submit=st.button(label='Search')
+    id = st.text_input('Your Code')
+    Submit = st.button(label='Search')
     if Submit:
-    	cursor.execute(f"select * from information where id='{id}'")
-    	tables = cursor.fetchall()
-    	st.write(tables)
+        cursor.execute(f"select * from information where id='{id}'")
+        tables = cursor.fetchall()
+        st.write(tables)
+
 
 def stat():
-    id=st.text_input('Your Id')
-    submit=st.button('Search',key='sub')
+    id = st.text_input('Your Id')
+    submit = st.button('Search', key='sub')
     if submit:
         cursor.execute(f"Select status from information where id='{id}'")
-        table=cursor.fetchall()
+        table = cursor.fetchall()
         st.write(table)
+
 
 def main():
     st.title('Diploma in Data Science Admission')
-    selected=st.sidebar.selectbox('Select',
-                        ('-----------',
-                        'Admin',
-                        'Registration',
-                        'Information',
-                        'Status'
-                        ))
-    if selected=='Admin':
+    selected = st.sidebar.selectbox('Select',
+                                    ('-----------',
+                                     'Admin',
+                                     'Registration',
+                                     'Information',
+                                     'Status'
+                                     ))
+    if selected == 'Admin':
         admin()
-    elif selected=='Registration':
+    elif selected == 'Registration':
         form()
-    elif selected=='Information':
+    elif selected == 'Information':
         info()
-    elif selected=='Status':
+    elif selected == 'Status':
         stat()
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
     main()
